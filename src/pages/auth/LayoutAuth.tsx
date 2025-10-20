7272b7bcfda99a1a91252cb583becb43721069e6
uncledev <arce.jhonbrian.dev@gmail.com>
2025-10-21 02:11:36 +0800

    Add padding to LayoutAuth component for improved responsiveness

--- src/pages/auth/LayoutAuth.tsx
+++ src/pages/auth/LayoutAuth.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 export default function LayoutAuth() {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center space-y-8">
+    <div className="min-h-screen flex flex-col justify-center items-center space-y-8 px-4 md:px-6 lg:px-8">
       <div className="flex flex-col items-center gap-1">
         <Logo size="6xl" />
         <p className="text-xs md:text-sm font-medium text-muted-foreground">
