97307d68b3232b6cb87367be5a6f8db828b68d6c
uncledev <arce.jhonbrian.dev@gmail.com>
2025-10-21 02:07:40 +0800

    Update vercel.json for rewrites, adjust base path in vite.config.ts, and add _redirects file

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/",
   plugins: [
     react(),
     tailwindcss(),
