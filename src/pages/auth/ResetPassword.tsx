e89a580558fbcfdb0734f60e5a26656f74fa7885
uncleedev <arce.jhonbrian.dev@gmail.com>
2025-10-21 01:00:31 +0800

    Revamp ResetPasswordPage layout and structure

--- src/pages/auth/ResetPassword.tsx
+++ src/pages/auth/ResetPassword.tsx
@@ -1,3 +1,25 @@
+import { Link } from "react-router-dom";
+
 export default function ResetPasswordPage() {
-  return <div>ResetPassword</div>;
+  return (
+    <div className="w-full max-w-md space-y-6">
+      <header>
+        <h2 className="text-xl font-semibold">Forgot Password</h2>
+        <p className="text-muted-foreground">
+          Welcome back! Please sign in to continue.
+        </p>
+      </header>
+
+      <form className="space-y-6"></form>
+
+      <footer className="flex flex-col items-center gap-2 text-sm">
+        <Link
+          to="/auth/forgot-password"
+          className="text-secondary hover:underline"
+        >
+          Forgot your password?
+        </Link>
+      </footer>
+    </div>
+  );
 }
