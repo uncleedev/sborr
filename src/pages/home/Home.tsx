db7b72b91c99c83ed5217885ef25966b1f596b38
uncledev <arce.jhonbrian.dev@gmail.com>
2025-10-21 01:06:59 +0800

    Add Signin link to HomePage component

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function HomePage() {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+      <Link to={"/auth/signin"}>Signin</Link>
+    </div>
+  );
 }
